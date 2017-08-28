dbd83408a4aa8819f4e81493bdd068d3da7aff4f
Marcelo Galvao <mcjgalvao@gmail.com>
2017-08-28 20:58:38 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+  <?php
+	echo "Oi. Estou aqui";
+  ?>
+</body>
+</html>
